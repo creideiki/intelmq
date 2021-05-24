ab34a73290b97b45b77e2272d37afd44570c8597
Christopher Gilmore-Ellis <christopher.gilmore-ellis@liu.se>
2021-05-24 09:30:53 +0200

    Add valid alert test, fix Mock class to handle status code and post requests

--- intelmq/tests/bots/collectors/defender/test_collector.py
+++ intelmq/tests/bots/collectors/defender/test_collector.py
@@ -12,6 +12,7 @@ from intelmq.bots.collectors.defender.collector_defender import DefenderCollecto
 
 class Mock_Response:
     text: str = ""
+    status_code = 200
 
     def __init__(self, structure):
         self.text = json.dumps(structure)
@@ -21,17 +22,21 @@ class Mock_API_Endpoint:
     api_uri = ""
     alert_response = []
     files_response = []
+    hunting_response = []
 
-    def __init__(self, api_uri="", alert_response=[], files_response=[]):
+    def __init__(self, api_uri="", alert_response=[], files_response=[], hunting_response=[]):
         self.api_uri = api_uri
         self.alert_response = alert_response
         self.files_response = files_response
+        self.hunting_response = hunting_response
 
-    def __call__(self, url):
+    def __call__(self, url, data = None):
         if url.startswith(self.api_uri + "/alerts"):
             return Mock_Response(self.alert_response)
         elif url.startswith(self.api_uri + "/files"):
             return Mock_Response(self.files_response)
+        elif url.startswith(self.api_uri + "/advancedqueries/run"):
+            return Mock_Response(self.hunting_response)
         else:
             return Mock_Response("Mock API called with unknown URL: " + url)
 
@@ -87,6 +92,40 @@ class TestDefenderCollectorBot(test.BotTestCase, unittest.TestCase):
                                     "raw": base64_encode(json.dumps(empty_alert))
                                 },
                                 path="unhandled")
+
+    @patch('requests_oauthlib.OAuth2Session.post')
+    @patch('requests_oauthlib.OAuth2Session.fetch_token')
+    @patch('requests_oauthlib.OAuth2Session.get')
+    def test_valid_alert(self, oauth2_get_mock, oauth2_fetch_token_mock, oauth2_post_mock):
+        g = GenerateAlert()
+        g.use_evidence = True
+        g.use_only_valid_category = True
+        g.use_relateduser = False
+
+        alert = g.create_alert()
+        advancedhuntingresponse = { "Results": [] }
+
+        oauth2_get_mock.side_effect = Mock_API_Endpoint(api_uri="https://api.securitycenter.windows.com/api",
+                                                        alert_response={"value": [alert]})
+        oauth2_post_mock.side_effect = Mock_API_Endpoint(api_uri="https://api.securitycenter.windows.com/api",
+                                                         hunting_response={"value": advancedhuntingresponse})
+        self.prepare_bot(destination_queues={
+            "_default": "default_output_queue",
+            "unhandled": ["unhandled_queue"]
+        })
+        self.run_bot()
+        self.assertOutputQueueLen(1, "_default")
+        self.assertOutputQueueLen(0, "unhandled")
+        # self.assertMessageEqual(0,
+        #                         {
+        #                             "__type": "Event",
+        #                             "extra.alert_string": json.dumps(alert),
+        #                             "feed.accuracy": 100.0,
+        #                             "feed.name": "Test Bot",
+        #                             "feed.url": "https://api.securitycenter.windows.com/api",
+        #                             "raw": base64_encode(json.dumps(alert))
+        #                         },
+        #                         path="unhandled")
 
 
 if __name__ == "__main__":  # pragma: no cover
