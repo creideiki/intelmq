e3195c5861c671dc75f33eeb8aaa0fbbfcc95d2f
Karl-Johan Karlsson <karl-johan.karlsson@liu.se>
2021-05-25 10:33:38 +0200

    Strip Defender collector of non-collector functionality
    Make the Defender collector behave like a proper IntelMQ collector,
    i.e. calling an API, receiving a blob, and sending that blob on in
    a Report.

--- intelmq/tests/bots/collectors/defender/collector_defender.py
+++ intelmq/tests/bots/collectors/defender/collector_defender.py
@@ -8,93 +8,6 @@ https://docs.microsoft.com/en-us/microsoft-365/security/defender-endpoint/expose
 for an app with permissions to at least Read all alerts and Run
 advanced queries.
 
-Defender wants to include quite a lot of information that doesn't fit
-in IntelMQ's default harmonisation, so it abuses the "extra" namespace
-to store its information.
-
-Defender creates alerts of a number of different categories. The bot
-will handle the ones specified in the parameter "valid_categories".
-Any others will be dumped as a JSON-formatted string in the event
-field "extra.alert_string" and sent to the IntelMQ output path
-specified as "invalid_path".
-
-Output structure:
-
-   "extra.defender_id": "Defender incident ID",
-   "extra.evidence": [
-      List of "evidence" structures. The format is fixed, but contains
-      the union of all fields ever used. Hence, most fields are null,
-      and which fields contain useful data depends on the type of
-      evidence, which is stored in the "entityType" field.
-
-      Structure:
-      {
-         "aadUserId": "",
-         "accountName": "",
-         "detectionStatus": "",
-         "domainName": "",
-         "entityType": "",
-         "evidenceCreationTime": "Timestamp",
-         "fileName": "",
-         "filePath": "",
-         "ipAddress": "",
-         "parentProcessCreationTime": "",
-         "parentProcessFileName": "",
-         "parentProcessFilePath": "",
-         "parentProcessId": "",
-         "processCommandLine": "",
-         "processCreationTime": "",
-         "processId": "",
-         "registryHive": "",
-         "registryKey": "",
-         "registryValue": "",
-         "registryValueType": "",
-         "sha1": "",
-         "sha256": "",
-         "url": "",
-         "userPrincipalName": "",
-         "userSid": ""
-      }
-   ]
-
-   "extra.fileinfo": [
-
-      List of "fileinfo" structures, one for each evidence object of
-      type "File". These are linked to the evidence structures through
-      the sha1 and sha256 hash values.
-
-      Structure:
-      {
-         "@odata.context": "https://api-eu.securitycenter.windows.com/api/$metadata#Files/$entity",
-         "determinationType": "",
-         "determinationValue": "Malware name, if known",
-         "fileProductName": "",
-         "filePublisher": "",
-         "fileType": "",
-         "globalFirstObserved": "Timestamp",
-         "globalLastObserved": "Timestamp",
-         "globalPrevalence": Integer,
-         "isPeFile": Boolean,
-         "isValidCertificate": "",
-         "issuer": "",
-         "md5": "Hash",
-         "sha1": "Hash",
-         "sha256": "Hash",
-         "signer": "",
-         "signerHash": "",
-         "size": Integer
-      }
-   ]
-
-   "extra.incident.status": "Defender's incident status",
-   "extra.malware.category": "Malware category",
-   "extra.malware.severity": "Malware severity",
-   "extra.time.resolved": "Timestamp when Defender considered this incident resolved",
-   "malware.name": "Malware name, if known",
-   "source.fqdn": "Hostname of computer generating alert",
-   "time.source": "Timestamp of the first event in this Defender incident"
-   "source.account": "Account running the malware"
-
 SPDX-FileCopyrightText: 2021 Linköping University <https://liu.se/>
 SPDX-License-Identifier: AGPL-3.0-or-later
 
@@ -115,18 +28,9 @@ lookback: integer, default rate_limit, get events for the last this
           time slice, and setting it lower will lose events between
           runs.
 
-rate_limit: integer, no default, number of seconds to sleep between
+rate_limit: integer, default 60, number of seconds to sleep between
             runs. Must be >= 2, since the API defaults to throttling
             clients connecting more than 100 times/minute.
-
-valid_categories: list of strings, default [ "malware",
-                  "unwantedsoftware", "ransomware", "exploit",
-                  "credentialaccess" ], event categories to send to
-                  the default pipeline.
-
-invalid_path: string, default "invalid", the IntelMQ destination queue
-              handling invalid alerts.
-
 """
 from intelmq.lib.bot import CollectorBot
 from intelmq.lib.utils import unzip, create_request_session
@@ -141,13 +45,12 @@ from typing import Optional, List
 
 
 class DefenderCollectorBot(CollectorBot):
+    api_region: Optional[str] = None
     tenant_id: Optional[str] = None
     client_id: Optional[str] = None
     client_secret: Optional[str] = None
     lookback: int = 0
-    valid_categories: List[str] = ["malware", "unwantedsoftware", "ransomware", "exploit", "credentialaccess"]
-    invalid_path: str = "invalid"
-    api_region: Optional[str] = None
+    rate_limit: int = 60
 
     def init(self):
         if BackendApplicationClient is None:
@@ -180,11 +83,6 @@ class DefenderCollectorBot(CollectorBot):
         self.resource_uri = f"https://api.{self.base_uri}"
         self.api_uri = f"https://{api_host}.{self.base_uri}/api"
         self.alert_path = "/alerts"
-        self.advanced_query_path = "/advancedqueries/run"
-
-    def add_if_present(self, report, out_field, struct, in_field):
-        if struct.get(in_field, None):
-            report.add(out_field, struct[in_field])
 
     def process(self):
         client = BackendApplicationClient(client_id=self.client_id)
@@ -217,97 +115,11 @@ class DefenderCollectorBot(CollectorBot):
             return
 
         for alert in alerts:
-            self.logger.debug("Considering alert: %s.", alert)
-            category = alert.get("category", "unknown")
-            if category.casefold() not in self.valid_categories:
-                event = self.new_event()
-                event.add("feed.url", self.api_uri)
-                event.add("raw", str(alert))
-                event.add("extra.alert_string", json.dumps(alert, indent=4))
-                self.send_message(event, path=self.invalid_path)
-                continue
-
-            fileinfo = []
-            if alert.get("evidence", None):
-                for evidence in alert["evidence"]:
-                    if evidence["entityType"].casefold() == "file" and \
-                       evidence.get("sha1", None):
-                        data = self.get_fileinformation(oauth, evidence["sha1"])
-                        if data:
-                            fileinfo.append(data)
-
-            username = "Unknown"
-            if alert.get("relatedUser", None) and \
-               alert["relatedUser"].get("userName", None):
-                username = alert["relatedUser"]["userName"]
-            else:
-                query = {"Query": f'DeviceEvents | where DeviceId == "{alert["machineId"]}" and ActionType == "AntivirusDetection" | project username=InitiatingProcessAccountName | limit 1'}
-                result = self.run_advancedhunting(oauth, query)
-                if "error" in result:
-                    self.logger.warning("Error fetching username for machine %s: %s.", alert["machineid"], data["error"])
-                if len(result) > 0:
-                    username = result[0]["username"] or "Unknown"
-
-            event = self.new_event()
-            event.add("source.account", username)
-
-            self.add_if_present(event, "extra.defender_id", alert, "id")
-            self.add_if_present(event, "source.fqdn", alert, "computerDnsName")
-            self.add_if_present(event, "extra.malware.severity", alert, "severity")
-            self.add_if_present(event, "malware.name", alert, "threatName")
-            self.add_if_present(event, "extra.malware.category", alert, "category")
-            self.add_if_present(event, "extra.incident.status", alert, "status")  # Check if failed?
-            self.add_if_present(event, "extra.evidence", alert, "evidence")
-            if len(fileinfo) > 0:
-                event.add("extra.fileinfo", fileinfo)
-
-            if alert.get("firstEventTime", None):
-                event.add("time.source", self.format_timestamp(alert["firstEventTime"]))
-            if alert.get("resolvedTime", None):
-                event.add("extra.time.resolved", self.format_timestamp(alert["resolvedTime"]))
-
-            event.add("feed.url", self.api_uri)
-            event.add("raw", str(alert))
-
-            self.send_message(event)
-
-    def format_timestamp(self, timestamp):
-        return DateTime.convert_from_format(timestamp.split('.')[0] + "+0000", "%Y-%m-%dT%H:%M:%S%z")
-
-    def get_fileinformation(self, oauth, sha1):
-        result = {}
-
-        self.logger.debug("Fetching file information for SHA1 %s.", str(sha1))
-        r = oauth.get(self.api_uri + "/files/" + str(sha1))
-        self.logger.debug("Status: %s, text: %s.", r.status_code, r.text)
-        try:
-            result = json.loads(r.text)
-            if "error" in result:
-                self.logger.warning("Error fetching file information for sha1 %s: %s.", sha1, result["error"])
-                result = {}
-        except json.decoder.JSONDecodeError as e:
-            self.logger.error("JSON error getting file information: %s, Raw: %s.", str(e), r.text)
-        except KeyError as e:
-            self.logger.error("Error getting file information: Key not found: %s, Raw: %s.", str(e), r.text)
-        finally:
-            return result
-
-    def run_advancedhunting(self, oauth, query):
-        result = []
-
-        self.logger.debug("Running advanced hunting query: %s.", json.dumps(query))
-        r = oauth.post(self.api_uri + self.advanced_query_path, data=json.dumps(query))
-        self.logger.debug("Status: %s, text: %s.", r.status_code, r.text)
-        try:
-            data = json.loads(r.text)
-            if data.get("Results", None):
-                result = data["Results"]
-        except json.decoder.JSONDecodeError as e:
-            self.logger.error("JSON error running advanced hunting query: %s, Raw: %s.", str(e), r.text)
-        except KeyError as e:
-            self.logger.error("Error running advanced hunting query: Key not found: %s, Raw: %s.", str(e), r.text)
-        finally:
-            return result
+            self.logger.debug("Fetched alert: %s.", alert)
+            report = self.new_report()
+            report.add("feed.url", self.api_uri)
+            report.add("raw", json.dumps(alert))
+            self.send_message(report)
 
 
 BOT = DefenderCollectorBot
--- intelmq/tests/bots/collectors/defender/test_collector.py
+++ intelmq/tests/bots/collectors/defender/test_collector.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
 import unittest
-import pathlib
 from copy import deepcopy
 from unittest.mock import MagicMock, patch
-
-from generatealert import GenerateAlert
 
 import intelmq.lib.test as test
 from intelmq.lib.utils import base64_encode
@@ -13,6 +10,11 @@ from intelmq.bots.collectors.defender.collector_defender import DefenderCollecto
 
 
 class Mock_Response:
+    """A mocked HTTP response, as from Requests.
+
+    Returns the JSON dump of the Python structure given when creating
+    the object.
+    """
     text: str = ""
     status_code = 200
 
@@ -21,26 +23,33 @@ class Mock_Response:
 
 
 class Mock_API_Endpoint:
+    """A mocked REST API endpoint, suitable for calling using
+    Requests.get().
+
+    Returns Mock_Response objects containing static strings.
+    """
     api_uri = ""
-    alert_response = []
-    files_response = []
-    hunting_response = []
+    responses = {}
 
-    def __init__(self, api_uri="", alert_response=[], files_response=[], hunting_response=[]):
+    def __init__(self, api_uri="", responses={}):
+        """Create a mocked API endpoint.
+
+        Parameters:
+        api_uri (str): Base URI of the API endpoint
+        responses {str: str}: hash of {route1: response1, ...}
+
+        A call to "api_uri/routeX" will receive a Mock_Response object
+        containing "responseX".
+        """
         self.api_uri = api_uri
-        self.alert_response = alert_response
-        self.files_response = files_response
-        self.hunting_response = hunting_response
+        self.responses = responses
 
-    def __call__(self, url, data = None):
-        if url.startswith(self.api_uri + "/alerts"):
-            return Mock_Response(self.alert_response)
-        elif url.startswith(self.api_uri + "/files"):
-            return Mock_Response(self.files_response)
-        elif url.startswith(self.api_uri + "/advancedqueries/run"):
-            return Mock_Response(self.hunting_response)
-        else:
-            return Mock_Response("Mock API called with unknown URL: " + url)
+    def __call__(self, url, data=None):
+        for prefix in self.responses.items():
+            if url.startswith(self.api_uri + "/" + prefix[0]):
+                return Mock_Response(prefix[1])
+
+        return Mock_Response("Mock API called with unknown URL: " + url)
 
 
 class TestDefenderCollectorBot(test.BotTestCase, unittest.TestCase):
@@ -52,8 +61,6 @@ class TestDefenderCollectorBot(test.BotTestCase, unittest.TestCase):
             "tenant_id": "test_tenant_id",
             "client_id": "test_client_id",
             "client_secret": "test_client_secret",
-            "rate_limit": 60,
-            "invalid_path": "unhandled"
         }
 
     @patch('requests_oauthlib.OAuth2Session.fetch_token')
@@ -75,87 +82,61 @@ class TestDefenderCollectorBot(test.BotTestCase, unittest.TestCase):
     @patch('requests_oauthlib.OAuth2Session.get')
     def test_empty_alert(self, oauth2_get_mock, oauth2_fetch_token_mock):
         empty_alert = {}
-        oauth2_get_mock.side_effect = Mock_API_Endpoint(api_uri="https://api.securitycenter.windows.com/api",
-                                                        alert_response={"value": [empty_alert]})
-        self.prepare_bot(destination_queues={
-            "_default": "default_output_queue",
-            "unhandled": ["unhandled_queue"]
-        })
-        self.run_bot(prepare=False)
-        self.assertOutputQueueLen(0, "_default")
-        self.assertOutputQueueLen(1, "unhandled")
-        self.assertMessageEqual(0,
-                                {
-                                    "__type": "Event",
-                                    "extra.alert_string": json.dumps(empty_alert),
-                                    "feed.accuracy": 100.0,
-                                    "feed.name": "Test Bot",
-                                    "feed.url": "https://api.securitycenter.windows.com/api",
-                                    "raw": base64_encode(json.dumps(empty_alert))
-                                },
-                                path="unhandled")
-
-    @patch('requests_oauthlib.OAuth2Session.post')
-    @patch('requests_oauthlib.OAuth2Session.fetch_token')
-    @patch('requests_oauthlib.OAuth2Session.get')
-    def test_valid_alert(self, oauth2_get_mock, oauth2_fetch_token_mock, oauth2_post_mock):
-        PATH = pathlib.Path(__file__).parent / 'valid_input.json'
-        with open(PATH) as handle:
-            ALERT_RAW = handle.read()
-
-        ALERT = json.loads(ALERT_RAW)
-        EVIDENCE = deepcopy(ALERT["evidence"])
-
-
-        oauth2_get_mock.side_effect = Mock_API_Endpoint(api_uri="https://api.securitycenter.windows.com/api",
-                                                        alert_response={"value": [ALERT]})
-
+        oauth2_get_mock.side_effect = Mock_API_Endpoint("https://api.securitycenter.windows.com/api",
+                                                        {"alerts": {"value": [empty_alert]}})
         self.run_bot()
         self.assertOutputQueueLen(1)
         self.assertMessageEqual(0,
                                 {
                                     "__type": "Report",
-                                    #"source.account": "user",
-                                    "extra.defender_id": "da813763521489057597_72863788520",
-                                    #"source.fqdn": "computer.example.com",
-                                    "extra.malware.severity": "Medium",
-                                    #"malware.name": "PUA:Win32/Vigua.A",
-                                    "extra.malware.category": "Malware",
-                                    "extra.incident.status": "Resolved",
-                                    "extra.evidence": EVIDENCE,
-                                    #"time.source": "2021-05-24T07:51:49+00:00",
-                                    "extra.time.resolved": "2021-05-24T07:51:49+00:00",
-                                    "feed.url": "https://api.securitycenter.windows.com/api",
                                     "feed.accuracy": 100.0,
                                     "feed.name": "Test Bot",
-                                    "raw": base64_encode(str(ALERT))
+                                    "feed.url": "https://api.securitycenter.windows.com/api",
+                                    "raw": base64_encode(json.dumps(empty_alert))
                                 })
 
-    @patch('requests_oauthlib.OAuth2Session.post')
     @patch('requests_oauthlib.OAuth2Session.fetch_token')
     @patch('requests_oauthlib.OAuth2Session.get')
-    def test_fuzz_alert(self, oauth2_get_mock, oauth2_fetch_token_mock, oauth2_post_mock):
-        loop = 100
-        for i in range(loop):
-            g = GenerateAlert()
-            g.use_evidence = True
-            g.use_only_valid_category = False
-            g.use_relateduser = True
-            g.use_real_threats = False
-            g.use_comments = True
+    def test_multiple_alerts(self, oauth2_get_mock, oauth2_fetch_token_mock):
+        alerts = [
+            {
+                "id": "abc123",
+                "category": "Test",
+                "evidence": [{
+                    "entityType": "User",
+                    "accountName": "test"
+                }]
+            },
+            {
+                "id": "def456",
+                "category": "Test",
+                "evidence": [{
+                    "entityType": "User",
+                    "accountName": "test2"
+                }]
+            }
+        ]
+        oauth2_get_mock.side_effect = Mock_API_Endpoint("https://api.securitycenter.windows.com/api",
+                                                        {"alerts": {"value": alerts}})
+        self.run_bot()
+        self.assertOutputQueueLen(2)
+        self.assertMessageEqual(0,
+                                {
+                                    "__type": "Report",
+                                    "feed.accuracy": 100.0,
+                                    "feed.name": "Test Bot",
+                                    "feed.url": "https://api.securitycenter.windows.com/api",
+                                    "raw": base64_encode(json.dumps(alerts[0]))
+                                })
+        self.assertMessageEqual(1,
+                                {
+                                    "__type": "Report",
+                                    "feed.accuracy": 100.0,
+                                    "feed.name": "Test Bot",
+                                    "feed.url": "https://api.securitycenter.windows.com/api",
+                                    "raw": base64_encode(json.dumps(alerts[1]))
+                                })
 
-            alert = g.create_alert()
-            advancedhuntingresponse = { "Results": [] }
-
-            oauth2_get_mock.side_effect = Mock_API_Endpoint(api_uri="https://api.securitycenter.windows.com/api",
-                                                            alert_response={"value": [alert]})
-            oauth2_post_mock.side_effect = Mock_API_Endpoint(api_uri="https://api.securitycenter.windows.com/api",
-                                                            hunting_response={"value": advancedhuntingresponse})
-            self.prepare_bot(destination_queues={
-                "_default": "default_output_queue",
-                "unhandled": "unhandled_queue"
-            })
-            self.run_bot(prepare=False)
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
